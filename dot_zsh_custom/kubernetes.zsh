2d6aeff93a4793a38eb7cc7c68b691f2444e5859
John Stegeman <john.stegeman@gmail.com>
2022-08-01 08:37:46 -0400

    Added rancher desktop plus some plugins for VS code

--- dot_zsh_custom/kubernetes.zsh
+++ dot_zsh_custom/kubernetes.zsh
@@ -4,3 +4,5 @@ fi
 export KUBECONFIG=~/.kube/config:~/.kube/config_main
 
 alias kubesh='_kubesh(){kubectl exec -n $1 $2 --stdin --tty -- /bin/bash; unset -f _kubesh; }; _kubesh' 
+
+export PATH=$HOME/.rd/bin:$PATH
