26def2d0693f8e9fe7032ddcbd5f0deb7f15e637
John Stegeman <john.stegeman@gmail.com>
2022-11-28 21:20:38 -0500

    Added onepassword cli to brew bundle; changed mcreate command to init my chezmoi config from github

--- dot_zsh_custom/executable_multipass.zsh
+++
@@ -1,7 +0,0 @@
-mssh() {
-ssh  `multipass info $1 --format json | jq -r  ".info.$1.ipv4[0]"`
- }
-
-mcreate() {
-    multipass launch -c 8 -m 16G -d 15G -n $1 --cloud-init ~/config/neo_init.yaml --mount ~/mounts:/mnt/software
-}
