25402004f05d7b65b6b0ec470e2031a270dd978e
John Stegeman <john.stegeman@gmail.com>
2023-02-07 07:28:47 -0500

    Changed brewup to include --no-quarantine and --greedy-auto-updates (instead of --greedy)

--- dot_zsh_custom/executable_homebrew.zsh
+++ dot_zsh_custom/executable_homebrew.zsh
@@ -1,2 +1,2 @@
 alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-alias brewup='brew update; brew upgrade --greedy; brew cleanup; brew doctor'
+alias brewup='brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor'
