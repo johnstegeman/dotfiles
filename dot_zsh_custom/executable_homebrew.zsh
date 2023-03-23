2dd53a6b209912a5c9c158603bd59cc882a787b4
John Stegeman <john.stegeman@gmail.com>
2023-03-23 09:24:31 -0400

    Fixed up the path issue and tidied up .zshrc

--- dot_zsh_custom/executable_homebrew.zsh
+++ dot_zsh_custom/executable_homebrew.zsh
@@ -1,8 +1,2 @@
 alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
 alias brewup='brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor'
-
-export PATH=":$PATH"
-
-# Add homebrew's sbin and bin directories to the path
-#
-export PATH=/opt/homebrew/sbin:/opt/homebrew/bin:~/bin:$PATH
