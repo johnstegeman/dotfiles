c4bbe8e5ed3f5b4d987de939a02ba59c8502ac4a
John Stegeman <john.stegeman@gmail.com>
2023-03-26 08:09:10 -0400

    Various cleanup, formatting, fixed desktop background on first install

--- dot_zsh_custom/executable_homebrew.zsh
+++ dot_zsh_custom/executable_homebrew.zsh
@@ -1,2 +1,4 @@
-alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-alias brewup='brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor'
+if [ -x "$(command -v brew)" ]; then
+    alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
+    alias bup='brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor'
+fi
