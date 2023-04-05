b597eb7e4eaf22336077f6a1623d48671cff9b43
John Stegeman <john.stegeman@gmail.com>
2023-04-05 14:55:16 -0400

    Refactored some stuff into a local config-file plugin, refactored .zsh_custom directory

---
+++ dot_zsh_custom/config_plugin/config-file.plugin.zsh
@@ -0,0 +1,85 @@
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# auto-load personal functions
+
+fpath=("${0:h}/../functions" "${fpath[@]}")
+autoload -Uz $fpath[1]/*(.:t)
+
+#########################
+#       Variables       #
+#########################
+
+pchf="${0:h}/patches"
+thmf="${0:h}/themes"
+GENCOMPL_FPATH="${0:h}/completions"
+
+ZSH_AUTOSUGGEST_USE_ASYNC=true
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_HISTORY_IGNORE="?(#c100,)"
+ZSH_AUTOSUGGEST_MANUAL_REBIND=set
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+FAST_ALIAS_TIPS_PREFIX="» $(tput setaf 6)"
+FAST_ALIAS_TIPS_SUFFIX="$(tput sgr0) «"
+HISTORY_SUBSTRING_SEARCH_FUZZY=set
+
+export HISTTIMEFORMAT="[%F %T]"
+export HISTFILESIZE=1000000000
+export HISTSIZE=1000000000
+export SAVEHIST=1000000000
+
+FZF_DEFAULT_OPTS="
+--border
+--height 80%
+--extended
+--ansi
+--reverse
+--cycle
+--bind ctrl-s:toggle-sort
+--bind 'alt-e:execute($EDITOR {} >/dev/tty </dev/tty)'
+--preview \"(bat --color=always {} || ls -l --color=always {}) 2>/dev/null | head -200\"
+--preview-window right:60%
+"
+FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git 2>/dev/null"
+
+#########################
+#       Aliases         #
+#########################
+
+alias -- -='cd -'
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+#########################
+#         Other         #
+#########################
+
+setopt append_history       # Allow multiple terminal sessions to all append to one zsh command history
+setopt no_beep              # don't beep on error
+setopt auto_cd              # If you type foo, and it isn't a command, and it is a directory in your cdpath, go there
+setopt prompt_subst         # enable parameter expansion, command substitution, and arithmetic expansion in the prompt
+setopt interactive_comments # Allow comments even in interactive shells (especially for Muness)
+setopt extended_history     # Save start time and duration of each command
+
+# Fuzzy matching of completions for when you mistype them:
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
+
+# Pretty completions
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+zstyle ':completion:*' use-cache true
+zstyle ':completion:*' rehash true
