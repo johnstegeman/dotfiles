e6e5cd7b1dcc95a3cf616bd6099664ce39d48e05
John Stegeman <john.stegeman@gmail.com>
2024-01-22 11:16:11 -0500

    Added installer script for neovim on ubuntu

---
+++ config/executable_makeneovim.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt install build-essential
+sudo apt-get install manpages-dev
+sudo apt remove --purge --auto-remove cmake
+sudo apt update && \
+sudo apt install -y software-properties-common lsb-release && \
+sudo apt clean all
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+sudo apt update
+sudo apt install kitware-archive-keyring
+sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF7F09730B3F0A4
+sudo apt update
+sudo apt install cmake
+sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo 
+sudo make install
