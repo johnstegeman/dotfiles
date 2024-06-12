8e5f5e1247bb501d4aee8aa842de0c2bbc459d8e
John Stegeman <john.stegeman@gmail.com>
2024-06-12 09:37:31 -0400

    Removed brew alias (no longer seems to be required with pyenv)

--- dot_zsh_custom/autoload/executable_homebrew.zsh
+++ dot_zsh_custom/autoload/executable_homebrew.zsh
@@ -1,6 +1,6 @@
 if [ -x "$(command -v brew)" ]; then
-    if [ -x "$(command -v pyenv)" ]; then
-        alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-    fi
+    #if [ -x "$(command -v pyenv)" ]; then
+    #    alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
+    #fi
     alias bup='brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor'
 fi
