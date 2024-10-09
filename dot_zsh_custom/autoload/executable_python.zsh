296684c44aac94721adc7c9a22218d10aa6d488b
John Stegeman <john.stegeman@gmail.com>
2024-10-09 13:06:34 -0400

    Cleaned up pyenv initialization. init - = path/shims/completions where --path is path/shims only

--- dot_zsh_custom/autoload/executable_python.zsh
+++ dot_zsh_custom/autoload/executable_python.zsh
@@ -4,7 +4,7 @@ if [ -x "$(command -v pyenv)" ]; then
 	
 	export PYENV_ROOT="$HOME/.pyenv"
 	export PATH="$PYENV_ROOT/bin:$PATH"
-	eval "$(pyenv init --path)"
+#	eval "$(pyenv init --path)"
 
 	eval "$(pyenv init -)"
 
