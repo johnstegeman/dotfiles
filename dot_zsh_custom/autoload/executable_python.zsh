3d65953728d052fd6216b50623b2e19adce0fd77
John Stegeman <john.stegeman@gmail.com>
2024-11-20 11:01:09 -0500

    Replaced pyenv with uv

--- dot_zsh_custom/autoload/executable_python.zsh
+++ dot_zsh_custom/autoload/executable_python.zsh
@@ -1,18 +1,3 @@
 ##### Python stuff
 
-if [ -x "$(command -v pyenv)" ]; then
-	
-	export PYENV_ROOT="$HOME/.pyenv"
-	export PATH="$PYENV_ROOT/bin:$PATH"
-#	eval "$(pyenv init --path)"
-
-	eval "$(pyenv init -)"
-
-	VIRTUAL_ENV_DISABLE_PROMPT=1
-   eval "$(pyenv virtualenv-init -)"
-#	~/.pyenv/versions/3.12.2/bin/virtualenvwrapper.sh
-
-
-	alias plo="pip list --outdated"
-
-fi
+eval "$(uv generate-shell-completion zsh)"
