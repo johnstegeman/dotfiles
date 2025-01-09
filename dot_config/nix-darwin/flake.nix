0336edfe2404d6ca921135495b40c5a0dd5ead33
John Stegeman <john.stegeman@gmail.com>
2025-01-09 16:32:51 -0500

    Initial refactor of nix setup

--- dot_config/nix-darwin/flake.nix
+++ dot_config/nix-darwin/flake.nix
@@ -1,61 +1,22 @@
 {
-  description = "Example nix-darwin system flake";
-
+  description = "my minimal flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # Where we get most of our software. Giant mono repo with recipes
+    # called derivations that say how to build software.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # nixos-22.11
+
+    # Controls system level software and settings including fonts
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
   };
-
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [pkgs.vim 
-         pkgs.coreutils
-         pkgs.eza
-         pkgs.wget
-         pkgs.asciinema
-         pkgs.awscli2
-         pkgs.htop
-         pkgs.httpie
-         pkgs.luarocks
-         pkgs.nmap
-         pkgs.subversion
-         pkgs.kubectl
-         pkgs.kubernetes-helm
-         pkgs.chezmoi
-         pkgs.git
-         pkgs.neovim
-         pkgs.tmux
-         pkgs.pkgconf
-         pkgs.nodejs
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#macosSystem
-    darwinConfigurations."macosSystem" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
+    darwinConfigurations.macosSystem = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      modules = [
+        ./modules/darwin
+      ];
     };
   };
 }
---
+++ dot_config/nix-darwin/modules/darwin/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+  # here go the darwin preferences and config items
+  # programs.zsh.enable = true;
+  environment = {
+    shells = [ pkgs.bash pkgs.zsh ];
+    systemPackages = [
+         pkgs.vim 
+         pkgs.coreutils
+         pkgs.eza
+         pkgs.wget
+         pkgs.asciinema
+         pkgs.awscli2
+         pkgs.htop
+         pkgs.httpie
+         pkgs.luarocks
+         pkgs.nmap
+         pkgs.subversion
+         pkgs.kubectl
+         pkgs.kubernetes-helm
+         pkgs.chezmoi
+         pkgs.git
+         pkgs.neovim
+         pkgs.tmux
+         pkgs.pkgconf
+         pkgs.nodejs
+    ];
+    # systemPath = [ "/opt/homebrew/bin" ];
+    # pathsToLink = [ "/Applications" ];
+  };
+  users.users.jstegeman.shell = pkgs.zsh;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  services.nix-daemon.enable = true;
+  # backwards compat; don't change
+  system.stateVersion = 4;
+}
