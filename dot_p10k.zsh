c58071052e69e59628059fce1e041d2e0a579e38
John Stegeman <john.stegeman@gmail.com>
2025-01-14 09:16:24 -0500

    Added python version and jenv to prompt

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -33,6 +33,10 @@
     # os_icon               # os identifier
     dir                     # current directory
     vcs                     # git status
+    virtualenv
+    pyenv
+    jenv
+    direnv
     prompt_char             # prompt symbol
   )
 
@@ -922,7 +926,7 @@
   # Python virtual environment color.
   typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=37
   # Don't show Python version next to the virtual environment name.
-  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=true
   # If set to "false", won't show virtualenv if pyenv is already shown.
   # If set to "if-different", won't show virtualenv if it's the same as pyenv.
   typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false
