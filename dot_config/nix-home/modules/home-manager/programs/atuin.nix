8850a1d75cb7ef54fdc18ce916321ac0a213f3dd
John Stegeman <john.stegeman@gmail.com>
2025-01-15 10:21:09 -0500

    Disable atuin's Control-R key and set default to full mode

--- dot_config/nix-home/modules/home-manager/programs/atuin.nix
+++ dot_config/nix-home/modules/home-manager/programs/atuin.nix
@@ -2,8 +2,13 @@
    programs.atuin = {
       enable = true;
       enableZshIntegration = true;
+      # Leave control-R for fzf to handle
+      flags = [
+         "--disable-ctrl-r"
+      ];
       settings = {
          enter_accept = true;
+         style = "full";
       };
   };
 }
