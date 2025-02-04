3a07711e3357b19e7fda2d9751851a9b877e0223
John Stegeman <john.stegeman@gmail.com>
2025-02-04 12:07:56 -0500

    Fix installNix script to run with zsh and not bash

--- bin/executable_installNix.sh
+++ bin/executable_installNix.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
