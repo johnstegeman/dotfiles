ff5dd28cb9579e707018df887a6a88b081a7b7d0
John Stegeman <john.stegeman@gmail.com>
2025-02-04 07:28:25 -0500

    Fix python autoloads

--- dot_zsh_custom/autoload/executable_python.zsh
+++ dot_zsh_custom/autoload/executable_python.zsh
@@ -1,5 +1,5 @@
 ##### Python stuff
 
-eval "$(uv generate-shell-completion zsh)"
-alias uvu="uv lock --upgrade; uv sync"
-alias plo="uv pip list --outdated"
+command -v uv     &> /dev/null    && eval "$(uv generate-shell-completion zsh)"
+command -v uv     &> /dev/null    && alias uvu="uv lock --upgrade; uv sync"
+command -v uv     &> /dev/null    && alias plo="uv pip list --outdated"
