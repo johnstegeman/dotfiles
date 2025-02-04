20a9842678dc23c6bb1e250cc134c9bcb1a66ec5
John Stegeman <john.stegeman@gmail.com>
2025-02-04 16:06:30 -0500

    Add control-# mapping for numbered items

--- dot_config/nix-home/modules/home-manager/programs/atuin.nix
+++ dot_config/nix-home/modules/home-manager/programs/atuin.nix
@@ -9,6 +9,8 @@
       settings = {
          enter_accept = true;
          style = "full";
+         # Use Ctrl-0 .. Ctrl-9 instead of Alt-0 .. Alt-9 UI shortcuts
+         ctrl_n_shortcuts = true;
       };
   };
 }
