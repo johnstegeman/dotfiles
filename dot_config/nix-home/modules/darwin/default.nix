0fd8d2c20cadad45f6d559b09e799144d9012ebb
John Stegeman <john.stegeman@gmail.com>
2025-05-31 11:23:44 -0400

    Migrate macos preferences to nix

--- dot_config/nix-home/modules/darwin/default.nix
+++ dot_config/nix-home/modules/darwin/default.nix
@@ -2,7 +2,82 @@
   # here go the darwin preferences and config items
   # programs.zsh.enable = true;
   system.primaryUser = "jstegeman";
-  environment = {
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  networking.computerName = "jstegeman-mac4";
+  networking.hostName = "jstegeman-mac4";
+
+  system.defaults = {
+
+    NSGlobalDomain = {
+      AppleShowAllExtensions = true;
+      NSNavPanelExpandedStateForSaveMode = true;
+    };
+
+    controlcenter = {
+      AirDrop = false;
+      BatteryShowPercentage = true;
+      Bluetooth = true;
+      Display = false;
+      FocusModes = true;
+      NowPlaying = false;
+      Sound = true;
+    };
+
+    dock = {
+      largesize = 104;
+      magnification = true;
+      minimize-to-application = true;
+      persistent-apps = [
+        {app = "/System/Applications/Launchpad.app";}
+        {spacer = {small = true;};}
+        {app = "/Applications/Arc.app";}
+        {app = "/Applications/Email.app";}
+        {spacer = {small = true;};}
+        {app = "/Applications/Ghostty.app";}
+        {app = "/Applications/Notion Calendar.app";}
+        {app = "/Applications/Slack.app";}
+        {spacer = {small = true;};}
+        {app = "/System/Applications/Messages.app";}
+        {app = "/Applications/Asana.app";}
+        {spacer = {small = true;};}
+        {app = "/Applications/Visual Studio Code.app";}
+        {app = "/Applications/Neo4j Desktop 2.app";}
+        {app = "/Applications/Neo4j Desktop.app";}
+        {spacer = {small = true;};}
+        {app = "/System/Applications/System Settings.app";}
+      ];
+      persistent-others = [
+        "/Users/jstegeman/Downloads"
+      ];
+      show-process-indicators = true;
+      show-recents = false;
+      tilesize = 55;
+    };
+
+    finder = {
+      AppleShowAllExtensions = true;
+      FXDefaultSearchScope = "SCcf";
+      FXPreferredViewStyle = "Nlsv";
+      NewWindowTarget = "Home";
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+
+    magicmouse.MouseButtonMode = "TwoButton";
+    menuExtraClock.ShowAMPM = true;
+    menuExtraClock.ShowDate = 1;
+    screensaver.askForPassword = true;
+    screensaver.askForPasswordDelay = 10;
+
+  };
+  
+  system.keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToEscape = true;
+  };
+
+   environment = {
     shells = [ pkgs.bash pkgs.zsh ];
     systemPackages = [
       pkgs.kanata
