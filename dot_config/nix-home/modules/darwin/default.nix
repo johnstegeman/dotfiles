12c3fa71d8bc258db19d75b903f24d4082053655
John Stegeman <john.stegeman@gmail.com>
2025-06-01 17:11:25 -0400

    Factor homebrew casks into their own file

--- dot_config/nix-home/modules/darwin/default.nix
+++ dot_config/nix-home/modules/darwin/default.nix
@@ -114,42 +114,8 @@
       caskArgs = {
         no_quarantine = true;
       };
-      casks = [
-        "arc" #arc-browser
-        "1password" #_1password-gui
-        "asana"
-        #"disk-inventory-x" #same
-        "figma"
-        "google-drive"
-        "grammarly-desktop"
-        "microsoft-auto-update"
-        "multipass"
-        "neo4j" #neo4j-desktop
-        "notion-calendar"
-        #"rancher"
-        "shottr"
-        # "temurin@21" #temurin-bin-23 or 24
-        "webex" #same
-        "zappy"
-        "minecraft" #same
-        #"slack" #same
-        #"iterm2"
-        #"visual-studio-code" #vscode
-        "bartender" #same
-        # "maccy" #same
-        "raycast" #same 
-        #"zoom" #zoom-us
-        "gimp" #gimp3
-        "1password-cli" #_1password-cli
-        #"google-cloud-sdk"
-        "ghostty" #same
-        "font-maple-mono-nf" #maple-mono.NF
-        # "keymapp" #same
-        "karabiner-elements" #same
-        # "intellij-idea-ce" #jetbrains.idea-community
-        # "pycharm-ce" #jetbrains.pycharm-community
-        "orbstack"
-      ];
+      casks = pkgs.callPackage ./casks.nix {};
+
       masApps = {
         "Edison Mail" = 1489591003;
       };
