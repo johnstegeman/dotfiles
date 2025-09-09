53de03205f0c3948cdfbcf1c2e8159c1a60ab4f2
John Stegeman <john.stegeman@gmail.com>
2025-09-09 07:25:47 -0400

    Add passthrough default to cm and br functions

--- dot_config/fish/functions/br.fish
+++ dot_config/fish/functions/br.fish
@@ -6,6 +6,7 @@ function br
          case up; brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor
          case clean; brew bundle --file ~/config/Brewfile --cleanup --zap
          case in; brew bundle --file ~/config/Brewfile
+         case '*'; brew $argv
       end
 
    end 
--- dot_config/fish/functions/cm.fish
+++ dot_config/fish/functions/cm.fish
@@ -6,6 +6,7 @@ function cm --description 'wrapper for chezmoi: ap or ed'
          case ap; chezmoi apply -v --no-pager -R=always
          case ed; code $(chezmoi source-path)
          case cd; cd $(chezmoi source-path)
+         case '*'; chezmoi $argv
       end
 
    end 
