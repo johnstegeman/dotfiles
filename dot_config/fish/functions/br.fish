3b5684ffbdac2c809ab2daa3e1caa7d6daebcb18
John Stegeman <john.stegeman@gmail.com>
2025-11-13 16:15:23 -0500

    Remove --no-quarantine option from brew now that its deprecated

--- dot_config/fish/functions/br.fish
+++ dot_config/fish/functions/br.fish
@@ -3,7 +3,7 @@ function br
    if using brew
 
       switch $argv[1]
-         case up; brew update; brew upgrade --no-quarantine --greedy-auto-updates; brew cleanup; brew doctor
+         case up; brew update; brew upgrade --greedy-auto-updates; brew cleanup; brew doctor
          case clean; brew bundle --file ~/config/Brewfile --cleanup --zap
          case in; brew bundle --file ~/config/Brewfile
          case '*'; brew $argv
